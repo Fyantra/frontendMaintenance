def149d29b587067d6bf3bb71be0706e837d5b74
Fyantra <andrimalalafyantra@gmail.com>
2024-09-10 11:27:28 +0300

    second commit
    nouveau projet

--- README.md
+++
@@ -1,2 +0,0 @@
-# frontendMaintenance
- Application de gestion pour la maintenance industrielle dans une entreprise textile
---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import Checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    Checker({ typescript: true }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
