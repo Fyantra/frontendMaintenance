8147d7b0694e3dcba9febbe82607c5758e92e274
Fyantra <andrimalalafyantra@gmail.com>
2025-01-09 08:48:03 +0300

    Refactorisation

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,9 +11,7 @@ import './assets/css/uppy.min.css'
 import './assets/css/jquery.steps.css'
 import './assets/css/jquery.timepicker.css'
 import './assets/css/quill.snow.css'
-import './assets/css/daterangepicker.css'
 import './assets/css/dataTables.bootstrap4.css'
-import './assets/css/fullcalendar.css'
 import './assets/css/style.css'
 
 import "datatables.net-bs5";
